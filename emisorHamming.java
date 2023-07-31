965cb5e51badc14a42e699014df85730505b443f
Franco-611 <69815571+Franco-611@users.noreply.github.com>
2023-07-31 12:36:34 -0600

    Intento de avances en hamming

---
+++ emisorHamming.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class emisorHamming {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese una trama en binario: ");
+        String input = scanner.nextLine();
+
+        // Verificar si la trama ingresada es válida (solo 0s y 1s)
+        if (!input.matches("[01]+")) {
+            System.out.println("Trama no válida. Ingrese solo 0s y 1s.");
+            return;
+        }
+
+        // Calcular la cantidad de bits de paridad necesarios
+        int r = calculateNumberOfParityBits(input.length());
+
+        // Crear un array de caracteres que contenga la trama y los bits de paridad
+        char[] hammingCode = generateHammingCode(input, r);
+
+        System.out.println("El mensaje en binario con la información adicional es: ");
+        System.out.println(hammingCode);
+    }
+
+    private static int calculateNumberOfParityBits(int dataLength) {
+        int r = 1;
+
+        // Usamos la fórmula de Hamming para calcular la cantidad de bits de paridad (r)
+        while (Math.pow(2, r) < dataLength + r + 1) {
+            r++;
+        }
+
+        return r;
+    }
+
+    private static char[] generateHammingCode(String input, int r) {
+        int dataLength = input.length();
+        int hammingCodeLength = dataLength + r;
+        char[] hammingCode = new char[hammingCodeLength];
+        int dataIndex = 0;
+        int parityIndex = 0;
+
+        // Llenar el array de hammingCode con "_", esto nos permite mantener los índices consistentes
+        for (int i = 0; i < hammingCodeLength; i++) {
+            if (i == (1 << parityIndex) - 1) {
+                hammingCode[i] = '_';
+                parityIndex++;
+            } else {
+                // Agregar el bit de la trama original en la posición actual del índice
+                hammingCode[i] = input.charAt(dataIndex);
+                dataIndex++;
+            }
+        }
+
+        // Calcular los bits de paridad
+        for (int i = 0; i < r; i++) {
+            int bitPosition = 1 << i;
+            int onesCount = 0;
+
+            // Calcular la cantidad de 1s en cada secuencia de bits correspondiente a cada bit de paridad
+            for (int j = bitPosition - 1; j < hammingCodeLength; j += 2 * bitPosition) {
+                for (int k = j; k < j + bitPosition && k < hammingCodeLength; k++) {
+                    if (hammingCode[k] == '1') {
+                        onesCount++;
+                    }
+                }
+            }
+
+            // Establecer el bit de paridad en función del número de 1s
+            hammingCode[bitPosition - 1] = (onesCount % 2 == 0) ? '0' : '1';
+        }
+
+        return hammingCode;
+    }
+}
---
+++ receptorHamming.py
@@ -0,0 +1,79 @@
+def calculate_parity_bits(data_length):
+    r = 1
+    while 2 ** r < data_length + r + 1:
+        r += 1
+    return r
+
+
+def is_power_of_two(n):
+    return n & (n - 1) == 0
+
+
+def detect_error(hamming_code):
+    error_bit = 0
+    r = calculate_parity_bits(len(hamming_code))
+
+    for i in range(r):
+        parity_bit = 2 ** i
+        if parity_bit <= len(hamming_code):
+            ones_count = sum(
+                int(hamming_code[j - 1]) for j in range(1, len(hamming_code) + 1) if (j & parity_bit) != 0
+            )
+            if ones_count % 2 != int(hamming_code[parity_bit - 1]):
+                error_bit += parity_bit
+
+    return error_bit
+
+
+def correct_error(hamming_code, error_bit):
+    if error_bit == 0:
+        return hamming_code
+
+    hamming_code = list(hamming_code)
+    if error_bit <= len(hamming_code):
+        hamming_code[error_bit - 1] = "1" if hamming_code[error_bit - 1] == "0" else "0"
+    return "".join(hamming_code)
+
+
+def generate_hamming_code(data, r):
+    hamming_code = ["0"] * (len(data) + r)
+    data_index, parity_index = 0, 0
+
+    for i in range(1, len(hamming_code) + 1):
+        if is_power_of_two(i):
+            hamming_code[i - 1] = "_"
+            parity_index += 1
+        else:
+            hamming_code[i - 1] = data[data_index]
+            data_index += 1
+
+    for i in range(r):
+        parity_bit = 2 ** i
+        for j in range(1, len(hamming_code) + 1):
+            if j & parity_bit:
+                if hamming_code[j - 1] != "_":
+                    hamming_code[parity_bit - 1] = str(
+                        (int(hamming_code[parity_bit - 1]) + int(hamming_code[j - 1])) % 2
+                    )
+
+    return "".join(hamming_code)
+
+
+def receive_message(encoded_message):
+    hamming_code = encoded_message.strip()
+    error_bit = detect_error(hamming_code)
+
+    if error_bit == 0:
+        print("No se detectaron errores. Trama recibida:")
+        print(hamming_code)
+    else:
+        corrected_hamming_code = correct_error(hamming_code, error_bit)
+        print("Se detectaron errores y se corrigieron.")
+        print(f"Posición del bit erróneo: {error_bit}")
+        print("Trama corregida:")
+        print(corrected_hamming_code)
+
+
+if __name__ == "__main__":
+    input_message = input("Ingrese el mensaje en binario con información adicional: ")
+    receive_message(input_message)
